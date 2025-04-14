b090d9d308b75394e5d062c3006e1c46ba93d2bd
Jeffrey <j.lo128456@gmail.com>
2025-04-14 19:18:59 +1000

    completed assigment

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,23 @@
 def write_file(file_name, file_content):
-    pass
+    file_name = str(file_name)
+    if file_name[-4:] != ".txt":
+        file_name += ".txt"
+    
+    with open(file_name, 'w', encoding='utf-8') as file:
+        file.write(file_content)
 
 def append_file(file_name, append_content):
-    pass
+    file_name = str(file_name)
+    if file_name[-4:] != ".txt":
+        file_name += ".txt"
+
+    with open(file_name, 'a', encoding='utf-8') as file:
+        file.write(append_content)    
 
 def read_file(file_name):
-    pass
+    file_name = str(file_name)
+    if file_name[-4:] != ".txt":
+        file_name += ".txt"
+
+    with open(file_name, 'r', encoding='utf-8') as file:
+        return file.read()     
